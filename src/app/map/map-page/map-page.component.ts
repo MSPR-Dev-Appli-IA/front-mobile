5e1acb06215cd7e42af8303e1a61dbbd11671969
Anna Tylkowska <tylkowskanna@gmail.com>
2023-04-16 23:49:34 +0200

    feat(map): added map service

--- src/app/map/map-page/map-page.component.ts
+++ src/app/map/map-page/map-page.component.ts
@@ -1,61 +1,40 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { environment } from '../../../environments/environment';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
-import { geojson } from './map-source';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Subscription } from 'rxjs';
+import { MapService } from '../map.service';
 
 @Component({
   selector: 'app-map-page',
   templateUrl: './map-page.component.html',
   styleUrls: ['./map-page.component.scss'],
 })
-export class MapPageComponent implements OnInit {
+export class MapPageComponent implements OnInit, OnDestroy {
   map!: mapboxgl.Map;
-  style = 'mapbox://styles/mapbox/streets-v11';
-  lat = 44.837789;
-  lng = -0.57918;
 
-  apiKey = environment.mapboxApiKey;
-
-    constructor() { }
+  breakpointSubscription = new Subscription();
+  constructor(
+    private responsive: BreakpointObserver,
+    private mapService: MapService
+  ) {}
 
   ngOnInit() {
-    this.map = new mapboxgl.Map({
-      accessToken: this.apiKey,
-      container: 'map',
-      style: this.style,
-      zoom: 13,
-      center: [this.lng, this.lat]
-    });
-    // Add map controls
-    this.map.addControl(new mapboxgl.NavigationControl());
-    this.map.on('load', () => {
-      this.map.loadImage(
-          "https://img.icons8.com/material/256/marker.png",
-          (error, image: any) => {
-            if (error) throw error;
-            this.map.addImage('custom-marker', image);
-          }
-      );
-      this.map.addSource('points', geojson);
+    this.map = this.mapService.init();
+    this.mapService.configure(this.map);
+    this.map.resize();
 
-      this.map.addLayer({
-        'id': 'points',
-        'type': 'symbol',
-        'source': 'points',
-        'layout': {
-          'icon-anchor': 'bottom',
-          'icon-offset': [0,75],
-          'icon-image': 'custom-marker',
-          'icon-size': 0.15,
-          'text-field': ['get', 'title'],
-          'text-font': [
-            'Open Sans Semibold',
-            'Arial Unicode MS Bold'
-          ],
-          'text-offset': [0, 1.25],
-          'text-anchor': 'top'
-        }
-      });
-    });
+    this.breakpointSubscription.add(
+      this.responsive
+        .observe([Breakpoints.HandsetPortrait, Breakpoints.Web])
+        .subscribe(_ => this.map.resize())
+    );
+  }
+
+  ngAfterViewInit() {
+    this.map.resize();
+  }
+
+  ngOnDestroy() {
+    this.breakpointSubscription.unsubscribe();
   }
 }
